0725f28d5f14a13ecf11ac71cc89083fd7966bd4
Manideep reddy Desham <md61004@corp.cyient.com>
2022-11-18 16:28:04 +0530

    hj

--- build_script.sh
+++ build_script.sh
@@ -1,24 +1,16 @@
 cd springboot-cont-a
 mvn clean compile install
-cp -a ./target/web-services.war ../Tomcat-working/service-ab.war   
-cd ../Tomcat-working
-docker build -t sabari_task/cont-a:v1 -f DockerfileContA .              
+cp -a ./target/web-services.war /workspace/img-a.war               
 
 cd ../springboot-cont-b
 mvn clean compile install
-cp -a ./target/web-services.war ../Tomcat-working/service-ab.war
-cd ../Tomcat-working
-docker build -t sabari_task/cont-b:v1 -f DockerfileContB . 
+cp -a ./target/web-services.war /workspace/img-b.war 
 
 cd ../springboot-cont-c
 mvn clean compile install
-cp -a ./target/web-services.war ../Tomcat-working/service-cd.war
-cd ../Tomcat-working
-docker build -t sabari_task/cont-c:v1 -f DockerfileContC .
+cp -a ./target/web-services.war /workspace/img-c.war 
  
 
 cd ../springboot-cont-d
 mvn clean compile install
-cp -a ./target\web-services.war ../Tomcat-working/service-cd.war 
-cd ../Tomcat-working
-docker build -t sabari_task/cont-d:v1 -f DockerfileContD .
+cp -a ./target\web-services.war /workspace/img-d.war
