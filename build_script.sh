e208bdd23f9d2eaedfcd92139f0cfb535f8195d5
Manideep reddy Desham <md61004@corp.cyient.com>
2022-11-21 12:43:30 +0530

    docker build

--- build_script.sh
+++ build_script.sh
@@ -1,18 +1,18 @@
 cd springboot-cont-a
 mvn clean compile install
-cp -a ./target/web-services.war /workspace/img-a.war               
+cp -a ./target/web-services.war /workspace/Tomcat-working/img-a.war               
 
 cd ../springboot-cont-b
 mvn clean compile install
-cp -a ./target/web-services.war /workspace/img-b.war 
+cp -a ./target/web-services.war /workspace/Tomcat-working/img-b.war 
 
 cd ../springboot-cont-c
 mvn clean compile install
-cp -a ./target/web-services.war /workspace/img-c.war 
+cp -a ./target/web-services.war /workspace/Tomcat-working/img-c.war 
  
 
 cd ../springboot-cont-d
 mvn clean compile install
-cp -a ./target/web-services.war /workspace/img-d.war
+cp -a ./target/web-services.war /workspace/Tomcat-working/img-d.war
 
 cd /
